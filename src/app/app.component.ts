77b995d0924def65d58e087cdca6cd023467a213
Adrian Alonso <Adrianalonso2001@gmail.com>
2025-06-05 16:03:50 +0200

    Library install Sucrox/TFM#36

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import '@adrian_alonso/component-library/tfm-button';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true
 })
 export class AppComponent {
   title = 'tfm-angular';
